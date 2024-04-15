f995871f41d2ab464bcd478b237c0cd817ae19e6
karanara <karanara@mail.uc.edu>
2024-04-14 22:36:12 -0400

    added methods in controller

--- src/main/java/com/example/microservice/accounts/audit/AuditAwareImpl.java
+++ src/main/java/com/example/microservice/accounts/audit/AuditAwareImpl.java
@@ -1,8 +1,15 @@
 package com.example.microservice.accounts.audit;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
-@Component("auditaware")
-public class AuditAwareImpl {
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
 
+	@Override
+	public Optional<String> getCurrentAuditor(){
+		return Optional.of("ACCOUNTS_MS");
+	}
 }
